af3e31eebe958149a26c9d74c3a90e27a4cfff5f
cxchris <107954743+cxchris@users.noreply.github.com>
2023-09-23 10:50:03 -0400

    1

--- node/api/webhook.js
+++ node/api/webhook.js
@@ -1,9 +1,11 @@
-
+import { logger } from '../lib/log.js';
 
 export const webhook = async (req, res) => {
   try {
     const formData = req.body;
     const name = formData.name;
+
+    logger.info(JSON.stringify(formData));
     // if (!name) {
     //   throw new Error('name cannot be empty');
     // }
@@ -13,7 +15,7 @@ export const webhook = async (req, res) => {
     // if (!isValidSignature) {
     //   throw new Error('Invalid signature');
     // }
-
+    
     const result = formData;
     res.success(result);
   } catch (error) {
